393b3c3b14a93fb3d9a6999d7d58acfce3a69d04
SuperQKi <146518675+SuperQKi@users.noreply.github.com>
2024-12-19 03:48:52 +0700

    Update assembler.py

--- assembler.py
+++ assembler.py
@@ -2,13 +2,17 @@
 from assembly_parser import assemly_parser
 from register_table import register_table
 from instruction_table import instruction_table
+from floatingPoint_table import floatingPoint_table
 
-file_in = open('riscv1.asm')
+# mở file chưa lệnh cần phân tích
+file_in = open('D:\\Tester\\riscv1.asm', 'r')
 content = []
+# chuyển dữ liệu trong file sang list
 for line in file_in:
     line = line.strip()
     content.append(line)
-parse_instructions = assemly_parser(register_table, instruction_table)
+# chuyển lệnh sang mã máy
+parse_instructions = assemly_parser(register_table, instruction_table, floatingPoint_table)
 parse_instructions.build_label_table(content)
 parse_instructions.first_pass(content)
 
