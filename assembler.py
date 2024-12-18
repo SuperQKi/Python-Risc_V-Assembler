b4f2eb5911f95549f565c730914b698f535498c4
SuperQKi <146518675+SuperQKi@users.noreply.github.com>
2024-12-19 04:00:07 +0700

    Update assembler.py

--- assembler.py
+++ assembler.py
@@ -4,17 +4,21 @@ from register_table import register_table
 from instruction_table import instruction_table
 from floatingPoint_table import floatingPoint_table
 
-# mở file chưa lệnh cần phân tích
-file_in = open('D:\\Tester\\riscv1.asm', 'r')
-content = []
-# chuyển dữ liệu trong file sang list
-for line in file_in:
-    line = line.strip()
-    content.append(line)
-# chuyển lệnh sang mã máy
-parse_instructions = assemly_parser(register_table, instruction_table, floatingPoint_table)
-parse_instructions.build_label_table(content)
-parse_instructions.first_pass(content)
+try:
+    print('Program running...')
+    # mở file chưa lệnh cần phân tích
+    file_in = open('D:\\Tester\\riscv1.asm', 'r')
+    # chuyển dữ liệu trong file sang list
+    content = [line.strip() for line in file_in]
+    # chuyển lệnh sang mã máy
+    parse_instructions = assemly_parser(register_table, instruction_table, floatingPoint_table)
+    parse_instructions.build_label_table(content)
+    parse_instructions.first_pass(content)
+
+    print('Program finished!')
+
+except Exception as e:
+    print(f'Program failed: {e}')
 
 
 
