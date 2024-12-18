5a3996c38dda52e06b316e269dd1eb719a9b41db
SuperQKi <146518675+SuperQKi@users.noreply.github.com>
2024-12-19 03:46:40 +0700

    Update riscv1.asm

--- riscv1.asm
+++ riscv1.asm
@@ -1,17 +1,63 @@
-add t1, t2, t5
-lbu t1, -100(t2)
-lhu t1, 40(t2)
-srai t1, t2, 3
-srai t1, t2, 4
-sltiu t1, t2, 0x7
-sltu t1, t2, t3
-addi t1, zero, 7
-addi t2, zero, -7
-lw t1, 40(t2)
-jal t1, loop
-lui t1, -12
-bne t1, t2, loop
-and t2, t0, t1     # t2 = t0 AND t1
-or t3, t0, t1      # t3 = t0 OR t1
-xor t4, t0, t1     # t4 = t0 XOR t1
-loop:
+addi t1, zero, 20
+addi t2, zero, 200
+addi t0, zero, 50
+
+fcvt.s.w ft1, t1
+fcvt.s.wu ft2, t2
+fcvt.w.s t0, ft3
+fcvt.wu.s t2, ft0
+fcvt.d.w ft4, t1
+fcvt.d.wu ft5, t2
+fcvt.w.d t1, ft3
+fcvt.wu.d t2, ft0
+
+fmv.x.s t2, ft4
+fmv.s.x ft4, t1
+
+feq.s t2, ft1, ft2
+flt.s t1, ft2, ft4
+fle.s t3, ft3, ft4
+fclass.s t3, ft1
+feq.d t2, ft1, ft2
+flt.d t1, ft2, ft4
+fle.d t3, ft3, ft4
+fclass.d t3, ft1
+
+fmin.s ft6, ft1, ft2
+fmax.s ft7, ft1, ft2
+fmin.d ft8, ft1, ft2
+fmax.d ft9, ft1, ft2
+
+fsgnj.s ft1, ft2, ft3
+fsgnjn.s ft3, ft4, ft5
+fsgnjx.s ft4, ft5,ft6
+fsgnj.d ft1, ft2, ft3
+fsgnjn.d ft3, ft4, ft5
+fsgnjx.d ft4, ft5,ft6
+
+fmadd.s ft4, ft5,ft6, ft1
+fnmadd.s ft4, ft5,ft6, ft1
+fnmsub.s ft5, ft3,ft2, ft1
+fmsub.s ft1, ft2,ft6, ft3
+fmadd.d ft4, ft5,ft6, ft1
+fnmadd.d ft4, ft5,ft6, ft1
+fnmsub.d ft5, ft3,ft2, ft1
+fmsub.d ft1, ft2,ft6, ft3
+
+fsqrt.s ft1, ft5
+fsqrt.d ft3, ft4
+
+fadd.s ft1, ft3, ft4
+fsub.s ft6, ft3, ft3
+fmul.s ft2, ft4, ft1
+fdiv.s ft1, ft3, ft5
+fadd.d ft1, ft3, ft4
+fsub.d ft6, ft3, ft3
+fmul.d ft2, ft4, ft1
+fdiv.d ft1, ft3, ft5
+
+lui t1, 0x10010
+fsw ft7, 8(t1)
+fsd ft7, 12(t1)
+flw ft1, 8(t1)
+fld ft2, 12(t1) 
